5737b48949f3fd4bfc9eb4548269d08e1f19d1e0
Xue2015 <1161048063@qq.com>
2022-09-24 16:31:07 +0800

    hi net 9-2请求数据

--- lib/page/profile_page.dart
+++ lib/page/profile_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bill_app/http/core/hi_error.dart';
+import 'package:flutter_bill_app/http/dao/ProfileDao.dart';
+import 'package:flutter_bill_app/model/profile_mo.dart';
+import 'package:flutter_bill_app/util/toast.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({Key? key}) : super(key: key);
@@ -8,6 +12,7 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  ProfileMo? _profileMo;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,5 +21,27 @@ class _ProfilePageState extends State<ProfilePage> {
         child: Text('我的'),
       ),
     );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _loadData();
+  }
+
+  void _loadData() async {
+    try {
+      ProfileMo result = await ProfileDao.get();
+      print(result);
+      setState(() {
+        _profileMo = result;
+      });
+    } on NeedAuth catch (e) {
+      print(e);
+      showWarnToast(e.message);
+    } on HiNetError catch (e) {
+      print(e);
+      showWarnToast(e.message);
+    }
   }
 }
